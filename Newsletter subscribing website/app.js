54db7f0c2768d2e72bfb05bb4eefc0aa486623e9
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-20 23:21:55 +0530

    Added modiles

--- Newsletter subscribing website/app.js
+++ Newsletter subscribing website/app.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const bp = require('body-parser');
+const request = require('request');
+const https = require('https');
 
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bp.urlencoded({ extended: false }))
+
+app.post("/",(req,res)=>{
+  console.log("info received");
+  res.send("Signup success "+req.body.first+" "+" "+req.body.second+" "+req.body.mail);
+  
+})
+
+app.get("/",(req,res)=>{
+  console.log("Server is running");
+  res.sendFile(__dirname+"/signup.html");
+})
+
+app.listen(3000);
