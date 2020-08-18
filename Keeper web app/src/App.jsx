bb8ba856de2ae762225cbad0acb5ca45e1c2f885
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-08-18 23:11:06 +0530

    Uploaded App.jsx

--- Keeper web app/src/App.jsx
+++ Keeper web app/src/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
-import Header from "./header";
-import Footer from "./footer";
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <Note />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
